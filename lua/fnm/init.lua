66534f2d41e36e28e807dec7337b2c4f12ec15eb
Ezekiel Warren <zekewarren@gmail.com>
2024-11-04 18:54:25 -0800

    feat: initial plugin implementation

---
+++ lua/fnm/init.lua
@@ -0,0 +1,84 @@
+local last_dir = nil
+
+local M = {}
+
+local function update_env_from_fnm_json(output)
+	local success, env_data = pcall(vim.json.decode, output)
+	if not success then
+		vim.notify('Failed to parse fnm env output: ' .. output, vim.log.levels.ERROR)
+		return
+	end
+
+	for name, value in pairs(env_data) do
+		vim.env[name] = value
+	end
+end
+
+local function spawn_fnm()
+	local stdout = ''
+	local stderr = ''
+	local stdout_pipe = vim.uv.new_pipe(false)
+	local stderr_pipe = vim.uv.new_pipe(false)
+
+	local handle = vim.uv.spawn('fnm', {
+		args = { 'env', '--json' },
+		stdio = { nil, stdout_pipe, stderr_pipe },
+	}, function(code, _)
+		if code ~= 0 then
+			vim.notify(
+				string.format('fnm exited with code %d: %s', code, stderr),
+				vim.log.levels.ERROR
+			)
+			return
+		end
+
+		vim.schedule_wrap(update_env_from_fnm_json)(stdout)
+	end)
+
+	if not handle then
+		vim.notify('Failed to spawn fnm', vim.log.levels.ERROR)
+		return
+	end
+
+	vim.uv.read_start(stderr_pipe, function(err, data)
+		if err then
+			vim.notify('Error reading stderr: ' .. err, vim.log.levels.ERROR)
+			return
+		end
+		if data then
+			stderr = stderr .. data
+		end
+	end)
+
+	vim.uv.read_start(stdout_pipe, function(err, data)
+		if err then
+			vim.notify('Error reading stdout: ' .. err, vim.log.levels.ERROR)
+			return
+		end
+		if data then
+			stdout = stdout .. data
+		end
+	end)
+end
+
+
+function M.update_fnm_env()
+	local current_dir = vim.uv.cwd()
+	if last_dir == current_dir then return end
+
+	last_dir = current_dir
+	spawn_fnm()
+end
+
+function M.setup(opts)
+	opts = opts or {}
+
+	vim.api.nvim_create_autocmd({ 'DirChanged' }, {
+		group = vim.api.nvim_create_augroup('FnmDirChanged', {}),
+		callback = function() M.update_fnm_env() end,
+	})
+
+	M.update_fnm_env()
+end
+
+return M
